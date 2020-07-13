05eed9a5fc1c96903570bc83974b0b15c969fa1f
daniel-arnauer <daniel.arnauer@accenture.com>
2020-07-13 16:53:43 +0200

    Api: Add Condition to check all assignments

--- api/src/service/domain/organization/user_disable.ts
+++ api/src/service/domain/organization/user_disable.ts
@@ -12,6 +12,10 @@ import * as UserEventSourcing from "./user_eventsourcing";
 import * as UserDisabled from "./user_disabled";
 import * as UserRecord from "./user_record";
 import * as GlobalPermissions from "../workflow/global_permissions";
+import * as Project from "../workflow/project";
+import * as Subproject from "../workflow/subproject";
+import * as Workflowitem from "../workflow/workflowitem";
+import { map } from "../../../result";
 
 export interface RequestData {
   userId: string;
@@ -29,6 +33,51 @@ export function validate(input: any): Result.Type<RequestData> {
 interface Repository {
   getUser(userId: string): Promise<Result.Type<UserRecord.UserRecord>>;
   getGlobalPermissions(): Promise<GlobalPermissions.GlobalPermissions>;
+  getAllProjects(): Promise<Project.Project[]>;
+  getSubprojects(projectId: string): Promise<Result.Type<Subproject.Subproject[]>>;
+  getWorkflowitems(
+    projectId: string,
+    subprojectId: string,
+  ): Promise<Result.Type<Workflowitem.Workflowitem[]>>;
+}
+
+async function checkAssignments(repository: Repository, userToDisable: string) {
+  let assignedProjects: Project.Project[] = [];
+  let assignedSubprojects: Subproject.Subproject[] = [];
+  let assignedWorkflowitems: Workflowitem.Workflowitem[] = [];
+
+  const projects = await repository.getAllProjects();
+  //assignedProjects = projects.filter((project) => project.assignee === userToDisable && project.status === "open");
+  for await (const project of projects) {
+    if (project.status === "closed") continue;
+    if (project.assignee === userToDisable) {
+      assignedProjects.push(project);
+    }
+    const subprojects = await repository.getSubprojects(project.id);
+    if (Result.isErr(subprojects)) continue;
+    for await (const subproject of subprojects) {
+      if (subproject.status === "closed") continue;
+      if (subproject.assignee === userToDisable) {
+        assignedSubprojects.push(subproject);
+      }
+      const workflowitems = await repository.getWorkflowitems(project.id, subproject.id);
+      if (Result.isErr(workflowitems)) continue;
+      for await (const workflowitem of workflowitems) {
+        if (workflowitem.status === "closed") continue;
+        if (workflowitem.assignee === userToDisable) {
+          assignedWorkflowitems.push(workflowitem);
+        }
+      }
+    }
+  }
+  if (
+    assignedProjects.length === 0 &&
+    assignedSubprojects.length === 0 &&
+    assignedWorkflowitems.length === 0
+  ) {
+    return false;
+  }
+  return { assignedProjects, assignedSubprojects, assignedWorkflowitems };
 }
 
 export async function disableUser(
@@ -43,16 +92,17 @@ export async function disableUser(
   const validationResult = validate(data);
   const intent: Intent = "global.disableUser";
   const currentGlobalPermissions = await repository.getGlobalPermissions();
+  const userToDisable = data.userId;
   // Create the new event:
   const userDisabled = UserDisabled.createEvent(source, publisher, {
-    id: data.userId,
+    id: userToDisable,
   });
 
   if (Result.isErr(validationResult)) {
     return new PreconditionError(ctx, userDisabled, validationResult.message);
   }
 
-  const userResult = await repository.getUser(data.userId);
+  const userResult = await repository.getUser(userToDisable);
   if (Result.isErr(userResult)) {
     return new PreconditionError(ctx, userDisabled, "Error getting user");
   }
@@ -80,6 +130,20 @@ export async function disableUser(
       });
     }
   }
+
+  // Check if user is assigned to project / subproject / workflowitem
+  const assignments = await checkAssignments(repository, userToDisable);
+  if (assignments) {
+    console.log(" yes this user is assigned somehow - error");
+    console.log(assignments);
+    return new PreconditionError(
+      ctx,
+      userDisabled,
+      "Error - This user is still assigned to some project/subproject/wf-item: ",
+    );
+  }
+  console.log(" no ");
+  console.log(assignments);
 
   const updatedUser = UserEventSourcing.newUserFromEvent(ctx, user, userDisabled);
   if (Result.isErr(updatedUser)) {
--- api/src/service/user_disable.ts
+++ api/src/service/user_disable.ts
@@ -1,6 +1,8 @@
 import { Ctx } from "../lib/ctx";
 import * as Result from "../result";
 import { ConnToken } from "./conn";
+import * as Cache from "./cache2";
+
 import { ServiceUser } from "./domain/organization/service_user";
 import * as UserDisable from "./domain/organization/user_disable";
 import { getGlobalPermissions } from "./global_permissions_get";
@@ -14,10 +16,21 @@ export async function disableUser(
   issuerOrganization: string,
   revokee: UserDisable.RequestData,
 ): Promise<void> {
-  const result = await UserDisable.disableUser(ctx, issuer, issuerOrganization, revokee, {
-    getUser: () => UserQuery.getUser(conn, ctx, issuer, revokee.userId),
-    getGlobalPermissions: async () => getGlobalPermissions(conn, ctx, issuer),
-  });
+  const result = await Cache.withCache(conn, ctx, async (cache) =>
+    UserDisable.disableUser(ctx, issuer, issuerOrganization, revokee, {
+      getUser: () => UserQuery.getUser(conn, ctx, issuer, revokee.userId),
+      getGlobalPermissions: async () => getGlobalPermissions(conn, ctx, issuer),
+      getAllProjects: async () => {
+        return cache.getProjects();
+      },
+      getSubprojects: async (pId) => {
+        return cache.getSubprojects(pId);
+      },
+      getWorkflowitems: async (pId, spId) => {
+        return cache.getWorkflowitems(pId, spId);
+      },
+    }),
+  );
   if (Result.isErr(result)) return Promise.reject(result);
 
   for (const event of result) {
